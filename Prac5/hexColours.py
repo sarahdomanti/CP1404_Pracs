982d52fc31e5cc1b5a72d51b5130e46d6cd90cbb
Sarah <sarah.domanti@my.jcu.edu.au>
2016-08-31 14:08:26 +1000

    Complete warm up and intermediate exercises

---
+++ Prac5/hexColours.py
@@ -0,0 +1,11 @@
+COLOUR_DICT = {"Black": "#000000", "Blue": "#0000ff", "Brown": "#a52a2a",
+               "Coral": "#ff7f50", "DarkGreen": "#006400", "HotPink": "#ff69b4",
+               "Orange": "#ffa500", "Pink": "#ffc0cb", "Plum": "#dda0dd", "Red": "#ff0000"}
+colour = input("Enter a colour: ").title()
+while colour == "":
+    print("Please enter a valid colour")
+    colour = input("Enter a colour: ").title()
+if colour in COLOUR_DICT:
+    print("{} is {} in hexadecimal".format(colour, COLOUR_DICT[colour]))
+else:
+    print("{} is not in the dictionary.".format(colour))
